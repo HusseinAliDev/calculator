353fefaa7f5f7aefdbc172fbb16d1deafedd9cfe
HusseinAliDev <133891203+HusseinAliDev@users.noreply.github.com>
2025-07-27 12:21:55 +0300

    New file dayTwo

---
+++ dayTwo.py
@@ -0,0 +1,15 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))HussH
+year = 2025 - age
+print(f"Your birth year is {year}")
+
+
+
+for i in range(1, 6):
+    print(f"Number: {i}")
+
+my_list = [5, 10, 15]
+total = 0
+for num in my_list:
+    total += num
+print("Sum:", total)
